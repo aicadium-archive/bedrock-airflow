ae1eea45c255933617b8f7328721ee7e8158ddf6
Han Qiao <qiao@basis-ai.com>
2019-08-20 18:13:14 +0800

    Remove bedrock hook

--- bedrock_plugin.py
+++ bedrock_plugin.py
@@ -1,6 +1,7 @@
 import json
 import time
 from builtins import super
+from datetime import datetime, timedelta
 
 from airflow.exceptions import AirflowException
 from airflow.hooks.http_hook import HttpHook
@@ -26,12 +27,16 @@ class RunPipelineOperator(BaseOperator):
         latest commit of the branch is fetched, else if None is specified,
         the commit version when the pipeline was created will be fetched
 
-    environment_id: str
+    environment_id : str
         The public id of the environment to run the pipeline in. This value
         can be obtained from Bedrock UI by clicking on run pipeline.
 
     status_poke_interval : int
         Interval to check the status of the pipeline
+
+    run_timeout : timedelta
+        Expected running time of the pipeline. Used as a hard timeout to
+        stop the operator.
     """
 
     RUN_PIPELINE_PATH = "/{}/pipeline/{{}}/run/".format(API_VERSION)
@@ -49,6 +54,7 @@ class RunPipelineOperator(BaseOperator):
         run_source_commit,  # specify branch for latest commit, e.g., 'master'
         environment_id,  # obtained from run pipeline page on Bedrock UI
         status_poke_interval=15,
+        run_timeout=timedelta(hours=1),
         **kwargs
     ):
         super().__init__(**kwargs)
@@ -58,6 +64,7 @@ class RunPipelineOperator(BaseOperator):
         self.run_source_commit = run_source_commit
         self.environment_id = environment_id
         self.status_poke_interval = status_poke_interval
+        self.run_timeout = run_timeout
 
     def execute(self, context):
         # Run the training pipeline
@@ -78,21 +85,18 @@ class RunPipelineOperator(BaseOperator):
             self.log.error("Failed to run pipeline")
             raise ex
 
-        data = json.loads(res.content)
-        pipeline_run_id = data["entity_id"]
+        pipeline_run_id = json.loads(res.content)["entity_id"]
         self.pipeline_run_id = pipeline_run_id  # Used for cleanup only
         self.log.info(
             "Pipeline successfully run, pipeline run ID: {}".format(pipeline_run_id)
         )
 
         # Poll pipeline run status
+        deadline = datetime.utcnow() + self.run_timeout
         get_hook = HttpHook(method="GET", http_conn_id=self.conn_id)
-        while True:
-            status = self._check_status(get_hook, pipeline_run_id)
-            if status == "Succeeded":
+        while datetime.utcnow() < deadline:
+            if self._check_status(get_hook, pipeline_run_id):
                 return
-            if status in ["Failed", "Stopped"]:
-                raise Exception("Pipeline run failed: {}".format(pipeline_run_id))
             time.sleep(self.status_poke_interval)
 
         self._cleanup_run(pipeline_run_id, post_hook=hook)
@@ -108,12 +112,18 @@ class RunPipelineOperator(BaseOperator):
         )
 
         if res.status_code != 200:
-            return "Running"
+            self.log.warning("Non-200 status check: {}".format(res.content))
+            return False
 
-        data = json.loads(res.content)
-        status = data["status"]
+        status = json.loads(res.content)["status"]
         self.log.info("Status of pipeline run: {}".format(status))
-        return status
+
+        if status in self.SUCCESS_STATUS:
+            return True
+        elif status in self.WAIT_STATUS:
+            return False
+        else:
+            raise Exception("Run status is {}".format(status))
 
     def _cleanup_run(self, pipeline_run_id, post_hook=None):
         self.log.info("Stopping pipeline run")
